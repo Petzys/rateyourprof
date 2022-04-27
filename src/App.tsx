e748185a452363e1f5ea4889c32387e39736826d
Petzys <bengtwegner@gmx.de>
2022-04-27 20:32:30 +0200

    Added Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Route, Routes, Navigate, Link } from "react-router-dom";
-
+import {BrowserRouter, Route, Routes, Navigate, Router, Link } from "react-router-dom";
+import React from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
 import './App.css';
+import Navbar from "./Navbar/index";
+
 function App() {
-  return (
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Navigate replace to='/home' />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-  );
+    return (
+        <div className={"App"}>
+            <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Navigate replace to='/home' />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+        </div>
+    );
 }
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import React from "react";
 function Home() {
     return (
         <div>
-            <h2>Home</h2>
-            <h2> New day, new try...</h2>
+            <h2>Willkommen bei Rate-your-Prof</h2>
+            <h2>Constantly evolving...</h2>
         </div>
     );
 }
